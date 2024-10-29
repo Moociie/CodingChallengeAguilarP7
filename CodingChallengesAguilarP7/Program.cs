754ad2e9f7a39f4051c7fda95d8849fc45b72af0
Kimberly Aguilar <757379@student.dusd.net>
2024-10-29 15:24:26 -0700

    fixed challenge 8
    challenge 8 works

--- CodingChallengesAguilarP7/Program.cs
+++ CodingChallengesAguilarP7/Program.cs
@@ -3,14 +3,14 @@ using System.Numerics;
 
 class Challenges
 {
-   
+
 
     public static void Main(string[] args)
     {
         //Welcome the user to my program
         Console.WriteLine("Welcome to my coding challenge program! I will be making a variety of functions for you to use! \n Please pick one.");
 
-        Console.WriteLine(" Sum \n Convert \n Increase \n CircuitPower \n AgeConvert \n TriArea \n lessThanOrEqualToZero \n lessThan100");
+        Console.WriteLine(" Sum \n Convert \n Increase \n CircuitPower \n AgeConvert \n TriArea \n lessThanOrEqualToZero \n lessThan100 \n IsEqual");
 
         Console.WriteLine("We are going to use the number adder today called sum. Please give me two numbers.");
         var number1 = Console.ReadLine();
@@ -22,15 +22,15 @@ class Challenges
         int numberToUse2 = int.Parse(number2);
 
         Console.WriteLine("The sum of the number: " + number1 + " and the number " + number2 + " is equal to " + Sum(numberToUse1, numberToUse2));
-        
+
         Sum(numberToUse1, numberToUse2);
 
         Console.WriteLine("I am going to convert minutes to seconds. \n Please enter a number to convert.");
 
-            var number3 = Console.ReadLine();
-            int numberToUse3 = int.Parse(number3);
+        var number3 = Console.ReadLine();
+        int numberToUse3 = int.Parse(number3);
 
-        Console.WriteLine("If I convert " + number3 + "minutes to seconds, I get " + Convert(numberToUse3) + " Seconds");
+        Console.WriteLine("If I convert " + number3 + " minutes to seconds, I get " + Convert(numberToUse3) + " Seconds");
 
         Console.WriteLine("Please insert a number. I will add 1 to that number.");
 
@@ -83,7 +83,15 @@ class Challenges
         int numberToUse12 = int.Parse(number12);
         Console.WriteLine("I have determined that the solution is " + lessThan100(numberToUse11, numberToUse12));
 
-       
+        Console.WriteLine("I will now determine if two integers are equal to eachother. \n Insert your first number.");
+        var number13 = Console.ReadLine();
+        int numberToUse13 = int.Parse(number13);
+        Console.WriteLine("Insert your next number.");
+        var number14 = Console.ReadLine();
+        int numberToUse14 = int.Parse(number14);
+        Console.WriteLine("I have determined that the solution is " + IsEqual(numberToUse13, numberToUse14));
+
+
 
     }
 
@@ -97,7 +105,7 @@ class Challenges
     }
     public static int Increase(int number4)
     {
-        return number4 + 1; 
+        return number4 + 1;
     }
     public static int CircuitPower(int number5, int number6)
     {
@@ -109,11 +117,11 @@ class Challenges
     }
     public static float TriArea(int Base, int Height)
     {
-        return ((Base * Height) /2);
+        return ((Base * Height) / 2);
     }
     public static bool LessThanOrEqualToZero(int number10)
     {
-        if(number10 <= 0)
+        if (number10 <= 0)
         {
             return true;
         }
@@ -124,7 +132,7 @@ class Challenges
     }
     public static bool lessThan100(int number11, int number12)
     {
-        if(number11 + number12 < 100)
+        if (number11 + number12 <= 100)
         {
             return true;
         }
@@ -133,7 +141,9 @@ class Challenges
             return false;
         }
     }
-
-
+    public static bool IsEqual(int number13, int number14)
+    {
+        return number13 == number14;
+    }
 }
 
